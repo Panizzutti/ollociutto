dcd245cd738efb98729f7121fbc1cd0b6e421030
Giorgio <giorgio.panizzutti@gmail.com>
2020-05-23 17:33:55 +0200

    olo

--- miane.py
+++ miane.py
@@ -2,3 +2,10 @@ import numpy as np
 import pandas as pd
 
 from pandas import ExcelFile
+
+df = pd.read_excel('covid.xlsx')
+a = df['dateRep'].unique()
+
+b = a['dateRep'].dt.strftime('%m/%d/%Y')
+
+print(sorted(a))
