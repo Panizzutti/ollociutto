897343e3447b8247ecabedd83e2de7350327344b
Giorgio <giorgio.panizzutti@gmail.com>
2020-05-24 20:39:08 +0200

    ciollamolla

--- pitone.py
+++ pitone.py
@@ -13,6 +13,7 @@ df = df.loc[~((df['countriesAndTerritories'] == 'Anguilla')
             | (df['countriesAndTerritories'] == 'Western_Sahara')
             | (df['countriesAndTerritories'] == 'Cases_on_an_international_conveyance_Japan')
             | (df['countriesAndTerritories'] == 'CuraÃ§ao')
+            | (df['countriesAndTerritories'] == 'Turks_and_Caicos_islands')
             | (df['countriesAndTerritories'] == '')
             | (df['countriesAndTerritories'] == '')
             | (df['countriesAndTerritories'] == '')
@@ -23,16 +24,27 @@ df = df.reset_index(drop= True )
 date = df['dateRep'].drop_duplicates()
 date = pd.to_datetime(date).sort_values().to_frame().rename(columns={"dateRep": "DATE"})
 
+listadate = date["DATE"]
+
+date = date.set_index("DATE")
 #date.to_csv('date.csv',index=False)
 
 countries = df['countriesAndTerritories'].drop_duplicates()
 
 #countries.to_csv('cane.csv', index=False)
+date = pd.concat([date,pd.DataFrame(columns=countries)])
+casi = date
+morti = date
+
+for n in range( df.shape[0]  ):
+    linea = df.iloc[n]
+
+    casi.at[ linea.dateRep, linea.countriesAndTerritories] = linea.cases
+    morti.at[ linea.dateRep, linea.countriesAndTerritories] = linea.deaths
 
 
-for n in range( df.shape[0]  ): 
-    print(countries)
-    
-
+date.to_csv('date.csv', index=False)
+casi.to_csv('casi.csv', index=True)
+morti.to_csv('morti.csv', index=False)
 
 
