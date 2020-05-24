7f7bf96076a32a92041afa42fc017690df40cb97
awasja <cannemozze22@gmail.com>
2020-05-24 13:25:33 +0200

    init

--- miane.py
+++ miane.py
@@ -5,6 +5,7 @@ from pandas import ExcelFile
 
 df = pd.read_excel('covid.xlsx')
 df = df.dropna() #rimozione anguilla e merde
-date = df['dateRep'].drop_duplicates()
-print(date)
-df.to_csv()
+df = df.reset_index(drop=True)
+date= df['dateRep'].drop_duplicates()
+rank = date.to_frame() #dataframe con le date univoche per il rank
+print(rank)
