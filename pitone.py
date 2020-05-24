7afa2ff9f3adb8cd8a0dd66ac821c4f664901943
Giorgio <giorgio.panizzutti@gmail.com>
2020-05-24 17:58:17 +0200

    sistemato

---
+++ pitone.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+from pandas import ExcelFile
+
+
+df = pd.read_excel('covid.xlsx')
+
+df = df.loc[~((df['countriesAndTerritories'] == 'Anguilla') 
+            | (df['countriesAndTerritories'] == 'Falkland_Islands_(Malvinas)')
+            | (df['countriesAndTerritories'] == 'Eritrea')
+            | (df['countriesAndTerritories'] == 'Bonaire, Saint Eustatius and Saba')
+            | (df['countriesAndTerritories'] == 'Western_Sahara')
+            | (df['countriesAndTerritories'] == 'Cases_on_an_international_conveyance_Japan')
+            | (df['countriesAndTerritories'] == 'CuraÃ§ao')
+            | (df['countriesAndTerritories'] == '')
+            | (df['countriesAndTerritories'] == '')
+            | (df['countriesAndTerritories'] == '')
+            ),:]
+
+date = df['dateRep'].drop_duplicates()
+date = pd.to_datetime(date)
+date = date.sort_values()
+
+date.rename(columns={"dateRep": "DATE"})
+
+date.to_csv('date.csv',index=False,)
